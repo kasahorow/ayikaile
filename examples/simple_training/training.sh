06e46fc5e341a52b95058bec176bb278517ba63e
Brady Fomegne <brady.fomegne@outlook.com>
2025-09-17 10:59:33 +0100

    make the example script follow the current API specs
    This commit handle the change in the transcription endpoint.
    - removing of target_language_code for the auto translation
    - adding of language_code to specify the langauge of the recording audio

--- examples/simple_training/training.sh
+++ examples/simple_training/training.sh
@@ -45,7 +45,7 @@ echo ''
 
 echo "Validation..."
 curl -X 'POST' \
-  "$HOST/api/v1/stt?target_language_code=akan&min_confidence=0.7&min_break_time=1000" \
+  "$HOST/api/v1/stt?language_code=akan&min_confidence=0.7&min_break_time=1000" \
   -H 'accept: application/json' \
   -H "Authorization: Bearer $token" \
   -H 'Content-Type: multipart/form-data' \
